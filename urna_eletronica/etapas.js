863aba1897e1a6d00c807d68955adef8b0fc5b1a
Ismael Mota <ismaelmmota@gmail.com>
2022-08-02 18:24:17 -0300

    formatacao do codigo usando prettier

--- urna_eletronica/etapas.js
+++ urna_eletronica/etapas.js
@@ -1,50 +1,54 @@
 let etapas = [
-    {
-    titulo: 'VEREADOR',
+  {
+    titulo: "VEREADOR",
     numeros: 5,
-    candidatos: [ 
-            { 
-                numero: '38111', 
-                nome: 'Goku-Pequeno', 
-                partido: 'Dragon Ball', 
-                fotos: [ 
-                    { url: 'Vereador-1.jpg', legenda: 'Vereador'}
-                ]
-            },
-            {
-                numero: '77222',
-                nome: 'Aang',
-                partido: 'Ultimo Mestre do Ar',
-                fotos: [
-                    {url: 'Vereador-2.jpg', legenda: 'Vereador'}
-                ]
-            },
-        ]
-    },
-    {
-        titulo: 'Prefeito',
-        numeros: 2,
-        candidatos: [
-            {
-                numero: '99',
-                nome: 'Fulano de Tal',
-                partido: 'ABCD',
-                vice: 'Fulano de Tal Vice',
-                fotos: [
-                    {url: 'candidato-prefeito-1.jpg', legenda: 'Prefeito'},
-                    {url: 'candidato-vice-prefeito-1.jpg', legenda: 'Vice-Prefeito', small: true}
-                ]
-            },
-            {
-                numero: '84',
-                nome: 'Beltrano de Tal',
-                partido: 'EFGH',
-                vice: 'Beltrano de Tal Vice',
-                fotos: [
-                    {url: 'candidato-prefeito-2.jpg', legenda: 'Prefeito'},
-                    {url: 'candidato-vice-prefeito-2.jpg', legenda: 'Vice-Prefeito', small: true}
-                ]
-            },
-        ]
-    }
+    candidatos: [
+      {
+        numero: "38111",
+        nome: "Goku-Pequeno",
+        partido: "Dragon Ball",
+        fotos: [{ url: "Vereador-1.jpg", legenda: "Vereador" }],
+      },
+      {
+        numero: "77222",
+        nome: "Aang",
+        partido: "Ultimo Mestre do Ar",
+        fotos: [{ url: "Vereador-2.jpg", legenda: "Vereador" }],
+      },
+    ],
+  },
+  {
+    titulo: "Prefeito",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: "99",
+        nome: "Fulano de Tal",
+        partido: "ABCD",
+        vice: "Fulano de Tal Vice",
+        fotos: [
+          { url: "candidato-prefeito-1.jpg", legenda: "Prefeito" },
+          {
+            url: "candidato-vice-prefeito-1.jpg",
+            legenda: "Vice-Prefeito",
+            small: true,
+          },
+        ],
+      },
+      {
+        numero: "84",
+        nome: "Beltrano de Tal",
+        partido: "EFGH",
+        vice: "Beltrano de Tal Vice",
+        fotos: [
+          { url: "candidato-prefeito-2.jpg", legenda: "Prefeito" },
+          {
+            url: "candidato-vice-prefeito-2.jpg",
+            legenda: "Vice-Prefeito",
+            small: true,
+          },
+        ],
+      },
+    ],
+  },
 ];
